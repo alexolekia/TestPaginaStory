fc614764b5e7269a578edbcdf43a71429c67b329
alexolekia <80401993+alexolekia@users.noreply.github.com>
2021-03-12 13:00:35 -0600

    Add files via upload
    RandomImages

--- assets/js/HideShow.js
+++ assets/js/HideShow.js
@@ -1,7 +1,33 @@
+var counterimg = 0;
+
+
 $(document).ready(function () {
     alert("La página cuenta con controles de estilo interactuables. Explora para elegir un tema adecuado. \n Todas las imágenes serán reemplazadas, así que ellas no cuentan para la paleta de colores")
     $(".HideShow").click(function (e) {
         $(".Controles").toggle();
     });
+    $(".imgSource").click(function (e) {
+        //$(document).getElementById("img").src = "http://placebear.com/600x600";
 
+        var imgsourcetype = counterimg % 3;
+        var seedimg = counterimg+345;
+        document.querySelectorAll('img').forEach((el) => {
+            var w = el.width;
+            var h = el.height;
+            
+
+            
+            if (imgsourcetype == 0) {
+                el.src = `http://placebear.com/${w}/${h}`;
+            } else if (imgsourcetype == 1) {
+                el.src = `http://placekitten.com/${w}/${h}`;
+            } else {
+                el.src = `https://picsum.photos/seed/${seedimg}/${w}/${h}?random=8`;
+                seedimg += 1;
+            };
+            
+            
+        });
+        counterimg+=1;
+    });
 });
