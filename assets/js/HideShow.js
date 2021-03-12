67318d875ed94feea80815b451b919de91c0e962
alexolekia <80401993+alexolekia@users.noreply.github.com>
2021-03-12 10:31:52 -0600

    Add files via upload
    Alerta al inicio

--- assets/js/HideShow.js
+++ assets/js/HideShow.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    alert("La página cuenta con controles de estilo interactuables. Explora para elegir un tema adecuado. \n Todas las imágenes serán reemplazadas, así que ellas no cuentan para la paleta de colores")
     $(".HideShow").click(function (e) {
         $(".Controles").toggle();
     });
